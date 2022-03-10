1fa7662fe455283a08fb0f2d54c021cfdec2fd3a
Qijun Jiang <hehefeel@gmail.com>
2022-03-10 09:45:42 -0500

    Initial Commit

---
+++ tester/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tester
+//
+//  Created by Qijun Jiang on 2022/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tester/testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  testerApp.swift
+//  tester
+//
+//  Created by Qijun Jiang on 2022/3/10.
+//
+
+import SwiftUI
+
+@main
+struct testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
